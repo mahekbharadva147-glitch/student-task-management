db20e2aadc4919bb522f85685a470ef867bf3a4b
mahek <mahekbharadva147@gmail.com>
2026-01-30 13:51:28 +0530

    [MB][ragister file is added]

---
+++ src/pages/Ragister.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Ragister = () => {
+  return (
+    <>
+      <div className="form-container">
+        <h1>Ragister</h1>
+            
+      </div>
+    </>
+  );
+};
+
+export default Ragister;
